f44855f7a590fe70a3d029a3cb7b8131d65fd9aa
David Snedecor <dsnedecor@gmail.com>
2016-10-01 21:11:39 -0500

    Adding extend-poly-lines

---
+++ extend-poly-lines.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @id             extend-poly-lines@dsnedecor
+// @name           IITC plugin: Extend Polygon Lines
+// @category       Layer
+// @version        0.0.1.20160929.141737
+// @updateURL      https://raw.githubusercontent.com/TheSned/IITCPlugins/master/extend-poly-lines.meta.js
+// @downloadURL    https://raw.githubusercontent.com/TheSned/IITCPlugins/master/extend-poly-lines.user.js
+// @description    Extends the lines of a polygon out past their vertices. Useful for determining which portals can be used for a layered field. drawTools Required.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
